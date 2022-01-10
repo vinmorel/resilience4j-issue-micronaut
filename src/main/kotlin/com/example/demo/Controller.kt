6962fd883a0ee0972e1fb959aab0ae92e571b73f
Vincent M <vincent.morel2@ubisoft.com>
2022-01-09 20:20:55 -0500

    removing unecessary packages to demo

--- src/main/kotlin/com/example/demo/Controller.kt
+++ src/main/kotlin/com/example/demo/Controller.kt
@@ -1,14 +1,11 @@
 package com.example.demo
 
-import io.github.resilience4j.micronaut.annotation.RateLimiter
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.Get
 
 
 @Controller("/")
-class Controller() {
-    @Get("/v1/dummyRequest")
+open class Controller() {
     fun dummyRequestV1(): HttpResponse<String> {
         return HttpResponse.ok("hello")
     }
